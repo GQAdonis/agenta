ddd432f778de8d98479ec12f4344feb8c18969b8
Akrem Abayed <aakrem@users.noreply.github.com>
2023-10-29 18:18:47 +0100

    add env var cypress

--- agenta-web/cypress.config.ts
+++ agenta-web/cypress.config.ts
@@ -1,15 +1,15 @@
 import {defineConfig} from "cypress"
 
+const OPENAI_API_KEY = process.env.CYPRESS_OPEN_AI_KEY || "your_api_key_here";
+
 export default defineConfig({
-    video: false,
-    screenshotOnRunFailure: false,
     e2e: {
         baseUrl: "http://localhost",
         defaultCommandTimeout: 8000,
     },
     env: {
         baseApiURL: "http://localhost/api",
-        OPENAI_API_KEY: "your_api_key_here",
+        OPENAI_API_KEY: OPENAI_API_KEY,
         localBaseUrl: "http://localhost",
         NEXT_PUBLIC_FF: false,
     },
